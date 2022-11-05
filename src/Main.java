f299a488ff5a90480a7031d4f93b50f94317e6c8
FelipeDePaula-Developer <felipe-dev@outlook.com>
2022-11-05 16:22:27 -0300

    Função que imprime os ids dos nos por nivel

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,29 @@ public class Main {
     public static void main(String[] args) {
         ArvoreBinaria a = new ArvoreBinaria(); // cria a árvore binária
 
-//        a.insere(555,"A - ROOT");
-//        a.insere(333,"B - ESQUERDA");
-//        a.insere(888,"C - DIREITA");
-//        a.insere(111,"D - ESQUERDA");
-//        a.insere(444,"E - DIREITA");
-//        a.insere(999,"F - DIREITA");
+        a.insere(555,"A - ROOT");
+        a.insere(333,"B - ESQUERDA");
+        a.insere(888,"C - DIREITA");
+        a.insere(111,"D - ESQUERDA");
+        a.insere(444,"E - DIREITA");
+        a.insere(999,"F - DIREITA");
 
-        a.insere(100, "A - Root");
-        a.insere(50, "B - esquerda");
-        a.insere(40, "C - esquerda");
-        a.insere(60, "D - direita");
-        a.insere(55, "E - esquerda");
-        a.insere(56, "F - direita");
-        a.insere(200, "G - direita");
-        a.insere(150, "H - esquerda");
-        a.insere(125, "I - esquerda");
-        a.insere(175, "J - direita");
-        a.insere(300, "K - direita");
-        a.insere(350, "L - direita");
+//        a.insere(100, "A - Root");
+//        a.insere(50, "B - esquerda");
+//        a.insere(40, "C - esquerda");
+//        a.insere(60, "D - direita");
+//        a.insere(55, "E - esquerda");
+//        a.insere(56, "F - direita");
+//        a.insere(200, "G - direita");
+//        a.insere(150, "H - esquerda");
+//        a.insere(125, "I - esquerda");
+//        a.insere(175, "J - direita");
+//        a.insere(300, "K - direita");
+//        a.insere(350, "L - direita");
 
 //        a.imprimeElementosArvore();
-        System.out.println(a.imprimeEsquerdaFestiva());
-        System.out.println("Altura: "+a.alturaArvore());
+//        System.out.println(a.imprimeEsquerdaFestiva());
+        a.imprimeNivelNo();
+//        System.out.println("Altura: "+a.alturaArvore());
     }
 }
